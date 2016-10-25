6c15e9e65032e85094474b853a079447a821605e
brueckmann <chrisnain@hotmail.com>
2016-10-25 16:54:10 +0200

    Added a controller.

---
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+	using System.Text.Encodings.Web;
+
+	public class HelloWorldController : Controller
+	{
+		// GET: /<controller>/
+		public string Index()
+		{
+			return "This is my default action...";
+		}
+
+		// GET: /<controller>/Welcome/{id}
+		public string Welcome(string name, int id = 1)
+		{
+			return HtmlEncoder.Default.Encode($"Hello {name}, id: {id}");
+		}
+	}
+}
